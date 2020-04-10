dd326c7669687d9786fb11de52fa80c4f00b1d55
delphic <delphic.uk@gmail.com>
2020-04-10 13:24:28 +0100

    Moved FileTab to it's own file, move config to a module and it's own file

---
+++ src/fileTab.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+
+var FileTab = module.exports = function() {
+    var exports = {};
+    var prototype = {
+        loadFile: function(filePath, callback) {
+        	if (filePath) {
+        	    let tab = this;
+        		fs.readFile(filePath, 'utf-8', function(error, data) {
+        			if (!error) {
+        			    tab.lastSavedData = data;
+        				tab.dataCache = data;
+        				tab.filePath = filePath;
+        			}
+    				callback(error, data);
+        		});
+	        }
+        },
+        saveFile: function(callback) {
+        	if (this.filePath) {
+        	    let tab = this;
+        		fs.writeFile(this.filePath, this.editor.getValue(), function(error) {
+        		    tab.lastSavedData = tab.editor.getValue();
+    		        if (callback) {
+    		            callback(error);
+    		        }
+        		});
+        	}
+        }
+        
+    };
+    var create = exports.create = function(params) {
+        var tab = Object.create(prototype);
+        tab.editor = params.editor;
+        tab.dataCache = "";
+        tab.filePath = "";
+        tab.lastSavedData = "";
+        tab.lastMessage = "Created new tab";
+        return tab;
+    };
+    
+    return exports;
+}();
